ef66e15a0a8e8fe241bf402b00a510dde6295d04
David Márquez <dmh.david@gmail.com>
2020-02-29 23:38:36 -0600

    Update to single pl

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-const path = require("path"),
-    fs = require("fs").promises,
-    yaml = require("js-yaml"),
-    core = require("@actions/core"),
+const core = require("@actions/core"),
     github = require("@actions/github");
 
 const REPO_DIRECTORY = process.env["GITHUB_WORKSPACE"],
-    CONFIG_PATH = path.join(REPO_DIRECTORY, getInput("config-path", ".github/source-dest.yml")),
-    token = core.getInput("github-token", { required: true }),
-    apply = getInput("apply", "never"),
+    targetBranch = core.getInput("target-branch", { required: true }),
+    allowedOrigens = core.getInput("allowed-branches", { required: true }),
     context = github.context,
     owner = context.repo.owner,
-    repo = context.repo.repo,
-    client = new github.GitHub(token);
+    repo = context.repo.repo;
 
 const getEvent = async () => JSON.parse(await fs.readFile(process.env["GITHUB_EVENT_PATH"]));
-
-async function getYamlConfig() {
-    try {
-        const text = await fs.readFile(CONFIG_PATH);
-        return yaml.safeLoad(text);
-    } catch (err) {
-        core.info(err); 
-        core.debug(err);
-        return undefined;
-    }
-}
-
-async function getConfig() {
-    const ymlConfig = await getYamlConfig();
-    return ymlConfig;
-}
 
 function getInput(name, fallback) {
     const input = core.getInput(name);
@@ -38,8 +17,6 @@ function getInput(name, fallback) {
 
 async function run() {
     try {
-        core.debug(JSON.stringify(context.payload));
-        core.debug(JSON.stringify(github.context));
         if (github.context.eventName != "pull_request") {
             core.info("This actions is only for pull request evaluation. Stepping out...");
             return;
@@ -52,27 +29,16 @@ async function run() {
     }
 }
 
-function labelMap(label) {
-    return label.name;
-}
-
-async function check() {
-    core.info(`In check`);
-    const state = context.payload.pull_request.state;
-    const config = await getConfig();
+async function check() {    
+    const state = context.payload.pull_request.state;    
 
     if (state != "open") {
         core.info("Pull request is not open. Stepping out...");
         return;
     }
 
-    if (!config) {
-        core.info("Config file not found. Stepping out...");
-        return;
-    }
-
     const target = context.payload.pull_request.base.ref;
-    const allowedBranches = config[target];
+    const allowedBranches = allowedOrigens;
     const source = context.payload.pull_request.head.ref;
 
     core.info(`Merge branch "${source}" into "${target}".`);
